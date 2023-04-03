e4f068a46b93852e3eb42553b2f96082541fb976
HEMSKI MIKHAIL <71402857+gem4ik@users.noreply.github.com>
2023-04-04 01:13:39 +0300

    JavaScript Algorithms and Data Structures
    FCC switch/ if/else

---
+++ JavaScript Algorithms and Data Structures/returning-boolean-values-from-functions.js
@@ -0,0 +1,14 @@
+function isLess(a, b) {
+  // Only change code below this line
+
+  return a < b;
+
+/*  if (a < b) {
+    return true;
+  } else {
+    return false;
+  } */
+  // Only change code above this line
+}
+
+isLess(10, 15);
